39438e4db2e0700254917652cd31b0b6c748dcdf
Deepesh <deepeshonearth@gmail.com>
2022-10-25 21:58:51 +0530

    Count-items-matching-a-rule.py added

---
+++ Count-items-matching-a-rule.py
@@ -0,0 +1,16 @@
+# items = [["phone","blue","pixel"],
+# ["computer","silver","lenovo"],
+# ["phone","gold","iphone"]], 
+# ruleKey = "color", ruleValue = "silver"
+
+def countItems(items, ruleKey, ruleValue):
+    ruleKeys = {"type":0, "color":1, "name":2}
+    count =0
+    for item in items:
+        if item[ruleKeys[ruleKey]] == ruleValue:
+            count +=1
+    return count
+print(countItems(items = [["phone","blue","pixel"],
+                          ["computer","silver","lenovo"],
+                          ["phone","gold","iphone"]], 
+                 ruleKey = "color", ruleValue = "silver"))
