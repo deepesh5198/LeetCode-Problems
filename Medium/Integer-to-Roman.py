b243ab0347262e49ed0e9a697e5c505abd2e63a2
Deepesh <deepeshonearth@gmail.com>
2022-12-04 19:27:27 +0530

    Medium/Integer-to-Roman.py added

---
+++ Medium/Integer-to-Roman.py
@@ -0,0 +1,27 @@
+num = 1994
+res = ""
+
+mapping = {
+            1000:'M',
+            900:'CM',
+            500:'D',
+            400:'CD',
+            100:'C',
+            90:'XC',
+            50:'L',
+            40:'XL',
+            10:'X',
+            9:'IX',
+            5:'V',
+            4:'IV',
+            1:'I'   
+}
+
+for k, val in mapping.items():
+    d = num//k 
+    num = num%k
+
+    res += val*d
+
+print(res)
+
