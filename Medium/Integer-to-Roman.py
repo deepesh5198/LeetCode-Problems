5f5f3e2cb0091a6c3180e03346969834f9860d4a
Deepesh <deepeshonearth@gmail.com>
2022-12-10 18:03:23 +0530

    Medium/Integer-to-Roman.py added

--- Medium/Integer-to-Roman.py
+++ Medium/Integer-to-Roman.py
@@ -1,27 +1,36 @@
-num = 1994
-res = ""
+# Problem Description:
+# Convert an Integer value to its Roman value
 
-mapping = {
-            1000:'M',
-            900:'CM',
-            500:'D',
-            400:'CD',
-            100:'C',
-            90:'XC',
-            50:'L',
-            40:'XL',
-            10:'X',
-            9:'IX',
-            5:'V',
-            4:'IV',
-            1:'I'   
-}
+# EXAMPLE:
 
-for k, val in mapping.items():
-    d = num//k 
-    num = num%k
+# Input: num = 1994
+# Output: "MCMXCIV"
+# Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
-    res += val*d
+def intToRoman(num: int) -> str:
+    res = ""
+    mapping = {
+                1000:'M',
+                900:'CM',
+                500:'D',
+                400:'CD',
+                100:'C',
+                90:'XC',
+                50:'L',
+                40:'XL',
+                10:'X',
+                9:'IX',
+                5:'V',
+                4:'IV',
+                1:'I'   
+    }
 
-print(res)
+    for k, val in mapping.items():
+        d = num//k 
+        num = num%k
+
+        res += val*d
+
+    return res
+
 
